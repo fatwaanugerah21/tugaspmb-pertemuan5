3aaa7c834d37b5196bf67a2441bb5db9087496e4
fatwaanugerah21 <fatwaanugerah21@gmail.com>
2021-03-24 23:44:30 +0800

    Kerja tugas PMB

---
+++ app/src/main/java/com/example/tugas4_pmb_h071191019_fatwaanugerahnasir/User.java
@@ -0,0 +1,15 @@
+package com.example.tugas4_pmb_h071191019_fatwaanugerahnasir;
+
+public class User {
+    public  String fullName, age, email;
+
+    public User(){
+
+    }
+
+    public  User(String fullName, String age, String email){
+        this.age = age;
+        this.fullName = fullName;
+        this.email = email;
+    }
+}
